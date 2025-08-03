7ea61fd80ff0bbbd4058adf72a8f2276d75e8c49
sajalkhan <sajalhossen@yahoo.com>
2025-08-03 08:45:44 +0600

    implement project with nextjs dynamic route

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to Next.js</h1>
-    </main>
+    <main className="flex flex-col items-center justify-between min-h-screen p-24"></main>
   );
 }
