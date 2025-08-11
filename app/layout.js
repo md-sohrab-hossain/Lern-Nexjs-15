7839e5e1574df26d8e7fc5e14353bea6a64d548d
sajalkhan <sajalhossen@yahoo.com>
2025-08-11 21:32:29 +0600

    implement weather application

--- app/layout.js
+++ app/layout.js
@@ -4,14 +4,14 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Next.js App",
-  description: "Created with Next.js",
+    title: "EcoVista",
+    description: "One Place Dashboard for Eco Information",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
+import LocationDetector from "@/components/LocationDetector";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to Next.js</h1>
-    </main>
-  );
+    return <LocationDetector />;
 }
